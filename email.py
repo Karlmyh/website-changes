3186e3fe52e4a1402307156b2b0b2c345154eaaf
Karlmyh <mayuhengkarl@gmail.com>
2022-09-01 02:49:45 -0400

    update

--- email.py
+++ email.py
@@ -1,28 +1,19 @@
-import smtplib
+def send_email():
+    smtpemail, smtppass, to, subject="mayuhengkarl@gmail.com","myh980510","mayuhengkarl@gmail.com","new pub!"
+    img_data = open(imgname, "rb").read()
+    msg = MIMEMultipart()
+    msg["Subject"] = subject
+    msg["From"] = smtpemail
+    msg["To"] = to
 
-def send_email(new_pub):
-    gmail_user = 'mayuheng@gmail.com'
-    gmail_password = 'Myh980510'
+    text = MIMEText(body)
+    msg.attach(text)
     
-    sent_from = gmail_user
-    to = ['person_a@gmail.com', 'person_b@gmail.com']
-    subject = 'Lorem ipsum dolor sit amet'
-    body = 'consectetur adipiscing elit'
-    
-    email_text = """\
-    From: %s
-    To: %s
-    Subject: %s
-    
-    %s
-    """ % (sent_from, ", ".join(to), subject, body)
-    
-    try:
-        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        smtp_server.ehlo()
-        smtp_server.login(gmail_user, gmail_password)
-        smtp_server.sendmail(sent_from, to, email_text)
-        smtp_server.close()
-        print ("Email sent successfully!")
-    except Exception as ex:
-        print ("Something went wrong….",ex)
+
+    s = smtplib.SMTP("smtp.gmail.com", "587")
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(smtpemail, smtppass)
+    s.sendmail(msg["From"], msg["To"], msg.as_string())
+    s.quit()
